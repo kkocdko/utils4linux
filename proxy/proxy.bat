2e77c17a33171c6f51ba8d3cbec1b0b5a240651b
kkocdko <kkocdko@gmail.com>
2024-09-28 03:14:39 +0800

    proxy: Merge server and client config into single file, add bench based on curl robots.txt

--- proxy/proxy.bat
+++ proxy/proxy.bat
@@ -2,6 +2,6 @@
 title sing-box
 fltmc>nul || echo Please run as administrator && pause && exit
 cd /d %~dp0
-curl -o config-client.jsonc -L "https://1.1.1.1:1111/dav/proxy/config.jsonc"
+curl -o config.jsonc -L "https://1.1.1.1:1111/dav/proxy/config.jsonc"
 sing-box version
-sing-box run -c config-client.jsonc -D . --disable-color
+sing-box run -c config.jsonc -D . --disable-color
