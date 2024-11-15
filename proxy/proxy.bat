328c5c0ae08d74e87da7b50d2efede5f059d2995
kkocdko <kkocdko@gmail.com>
2024-11-16 03:12:02 +0800

    proxy: Modify template, update to sing-box-1.11.0-alpha.18

--- proxy/proxy.bat
+++ proxy/proxy.bat
@@ -3,5 +3,4 @@ title sing-box
 fltmc>nul || echo Please run as administrator && pause && exit
 cd /d %~dp0
 curl -o config.jsonc -L "https://1.1.1.1:1111/dav/proxy/config.jsonc"
-sing-box version
 sing-box run -c config.jsonc -D . --disable-color
