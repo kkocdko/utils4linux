a09ceabc40493b6f62250dd5876d534f7acaa71b
kkocdko <kkocdko@gmail.com>
2026-07-13 00:10:31 +0800

    proxy: Update template and config-sub-fetch.js

--- proxy/config-sub-fetch.js
+++ proxy/config-sub-fetch.js
@@ -10,21 +10,26 @@ const gen = async (/** @type {string} */ sub) => {
   const res = await fetch(url, { headers: { "User-Agent": ua } });
   const obj = await res.json();
   const outbounds = [];
-  const MAX_I = 4;
+  const MAX_I = 5;
   const store = [
     ["emby", { i: 1, r: /emby/ }],
-    ["hk", { i: 1, r: /hong\-?kong|香港|🇭🇰/ }],
-    ["tw", { i: 1, r: /tai\-?wan|台湾|🇹🇼/ }],
-    ["sg", { i: 1, r: /singapore|新加坡|狮城|🇸🇬/ }],
-    ["in", { i: 1, r: /india|印度|🇮🇳/ }],
-    ["jp", { i: 1, r: /japan|日本|🇯🇵/ }],
-    ["kr", { i: 1, r: /korea|韩国|🇰🇷/ }],
-    ["lu", { i: 1, r: /luxembourg|卢森堡|🇱🇺/ }],
-    ["au", { i: 1, r: /australia|澳大利亚|🇦🇺/ }],
-    ["nl", { i: 1, r: /netherlands|荷兰|🇳🇱/ }],
-    ["us-d", { i: 1, r: /美国直连/ }],
-    ["us", { i: 1, r: /united\-?states|美国|🇺🇸/ }],
-    ["uk", { i: 1, r: /united\-?kingdom|英国|🇬🇧/ }],
+    ["jp-d", { i: 1, r: /\[直连\] 日本/ }],
+    ["sg-d", { i: 1, r: /\[直连\] 新加坡/ }],
+    ["us-d", { i: 1, r: /\[直连\] 美国/ }],
+    ["de-d", { i: 1, r: /\[直连\] 德国/ }],
+    ["hk", { i: 1, r: /^hk\-|hong\-?kong|香港|🇭🇰/ }],
+    ["mo", { i: 1, r: /^mo\-|macao|澳门|🇲🇴/ }],
+    ["tw", { i: 1, r: /^tw\-|tai\-?wan|台湾|🇹🇼/ }],
+    ["sg", { i: 1, r: /^sg\-|singapore|新加坡|狮城|🇸🇬/ }],
+    ["in", { i: 1, r: /^in\-|india|印度|🇮🇳/ }],
+    ["jp", { i: 1, r: /^jp\-|japan|日本|🇯🇵/ }],
+    ["kr", { i: 1, r: /^kr\-|korea|韩国|🇰🇷/ }],
+    ["lu", { i: 1, r: /^lu\-|luxembourg|卢森堡|🇱🇺/ }],
+    ["au", { i: 1, r: /^au\-|australia|澳大利亚|🇦🇺/ }],
+    ["nl", { i: 1, r: /^nl\-|netherlands|荷兰|🇳🇱/ }],
+    ["fr", { i: 1, r: /^fr\-|france|法国|🇫🇷/ }],
+    ["us", { i: 1, r: /^us\-|united\-?states|美国|🇺🇸/ }],
+    ["uk", { i: 1, r: /^uk\-|united\-?kingdom|英国|🇬🇧/ }],
   ];
   let ret = "// " + JSON.stringify(obj.outbounds.map((e) => e.tag)) + "\n";
   for (const e of obj.outbounds.filter((e) => e.server)) {
@@ -36,19 +41,21 @@ const gen = async (/** @type {string} */ sub) => {
     if (!found) continue;
     e.tag = name + "-" + found[0] + "-" + found[1].i++;
     if (factor && parseFloat(factor) !== 1) e.tag += "-x" + factor;
+    // if (e.tag.includes("[专线]")) continue;
+    if (e.type === "anytls") continue; // 兼容 sing-box 1.11
     outbounds.push(e);
   }
   ret += "// " + JSON.stringify(outbounds.map((e) => e.tag)) + "\n";
   const order = ["tag", "detour", "type", "server", "server_port"].reverse();
   const exclude = ["domain_resolver", "down_mbps", "up_mbps"]; // always bbr
   for (const outbound of outbounds) {
-    if (outbound.type === "anytls") continue; // 兼容 sing-box 1.11
     delete outbound.domain_resolver;
     const entries = Object.entries(outbound)
       .filter(([k]) => !exclude.includes(k))
       .sort(([a], [b]) => order.indexOf(b) - order.indexOf(a));
     ret += JSON.stringify(Object.fromEntries(entries)) + ",\n";
   }
+  console
   console.timeEnd(name);
   return ret;
 };
